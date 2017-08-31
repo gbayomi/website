52cad933bb15e32ee05b159e902335bfab2c4e51
Gabriel Bayomi Tinoco Kalejaiye <gbayomi@Gabriels-MacBook-Pro.local>
2017-08-31 11:29:04 -0400

    indexing resume

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.get('/~gbayomi/:id', function(req, res){
+	var par_id = req.params.id;
+ 	par_id = par_id.replace('\+', '\/')
+    var redi = '/' + par_id;
+    res.redirect(redi)
+});
+
+app.get('/~gbayomi', function(req, res){
+    res.redirect('/')
+});
+
+
 app.use('/', express.static(__dirname));
 
 app.listen(app.get('port'), function() {
